18faeb4b7021042ccdbfe32e2d875517fa56405c
Tjeepot <tjeepot@gmail.com>
2023-08-30 02:23:59 +0100

    fix os path

--- get_data.py
+++ get_data.py
@@ -14,7 +14,7 @@ __status__     = "Production"  # "Development" "Prototype" "Production"
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 import requests
 
