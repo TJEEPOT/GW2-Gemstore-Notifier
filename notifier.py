7a3546ff8d5f598f1c4eeaf8c00a5abcb77eefd3
Tjeepot <tjeepot@gmail.com>
2024-02-03 00:59:34 +0000

    Change program to create notifier.log if it doesn't exist

--- notifier.py
+++ notifier.py
@@ -74,7 +74,7 @@ def notify_discord(items:list[list[str]], discord_webhook:str, last_updated:str)
 def write_to_log(line:str):
     now = datetime.datetime.now()
     time = now.strftime("%b %d %H:%M:%S")
-    with open("notifier.log", "a") as f:
+    with open("notifier.log", "a+") as f:
         f.write(f"\n{time}: {line}")
 
 
